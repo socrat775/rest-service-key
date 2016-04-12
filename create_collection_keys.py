4e9e9b478d86d952ec14be1df8b9c465414c2918
socrat775 <socrat775@users.noreply.github.com>
2016-04-12 11:22:49 +0300

    Update create_collection_keys.py
    changed how created collection

--- create_collection_keys.py
+++ create_collection_keys.py
@@ -1,15 +1,18 @@
 import pymongo
 from string import ascii_letters
-
+from random import shuffle
 
 db = pymongo.MongoClient().test
 all_symbol = ascii_letters + ''.join(str(i) for i in xrange(0, 10))
 db.all_keys.create_index([('key', pymongo.ASCENDING),
                           ('status', pymongo.ASCENDING)], unique=True)
-
+keys = []
 for _1 in all_symbol:
    for _2 in all_symbol:
       for _3 in all_symbol:
          for _4 in all_symbol:
-            db.all_keys.save({'key': ''.join((_1, _2, _3, _4)), 'status': 0})
+            keys.append(''.join((_1, _2, _3, _4)))
 
+shuffle(keys)
+for key in keys:
+    db.all_keys.save({'key': key, 'status': 0})
